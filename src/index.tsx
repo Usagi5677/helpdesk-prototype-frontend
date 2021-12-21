93710a18dfc39f941c2462df7329f83b6a1ff6e8
Usagi88 <ibrahimnaish5677@gmail.com>
2021-12-21 18:44:11 +0500

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom";
+
+
+const App = () => {
+    return(
+        <div>
+            <h1>Help Desk - Hello world</h1>
+        </div>
+    )
+};
+
+ReactDOM.render(
+    <App></App>,
+    document.querySelector('#root')
+);
+
