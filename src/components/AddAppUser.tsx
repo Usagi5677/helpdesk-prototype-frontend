a213f1bb63108b7c103cde75e936e9a3cf7285e1
Usagi88 <ibrahimnaish5677@gmail.com>
2022-02-07 11:19:52 +0500

    fix: css import issue

--- src/components/AddAppUser.tsx
+++ src/components/AddAppUser.tsx
@@ -1,10 +1,10 @@
 import { useMutation } from "@apollo/client";
 import { Button, Checkbox, Col, Form, message, Modal, Row } from "antd";
-import "antd/lib/button/style/css";
-import "antd/lib/form/style/css";
-import "antd/lib/modal/style/css";
-import "antd/lib/message/style/css";
-import "antd/lib/checkbox/style/css";
+import "antd/lib/button/style/index.css";
+import "antd/lib/form/style/index.css";
+import "antd/lib/modal/style/index.css";
+import "antd/lib/message/style/index.css";
+import "antd/lib/checkbox/style/index.css";
 import { useState } from "react";
 import { useForm } from "antd/lib/form/Form";
 import SearchAPSUser from "./common/SearchAPS";
