95b2fa946e66c6eec1f80f6f8721504bc8855941
mismaah <mismaahabdulla@gmail.com>
2022-02-24 21:25:06 +0500

    MISC: Pass ws url as env variable

--- src/api/client.ts
+++ src/api/client.ts
@@ -16,7 +16,7 @@ function createApolloClient(uri: string | undefined) {
 
   const token = localStorage.getItem("helpdesk_token");
   const wsLink = new WebSocketLink({
-    uri: `wss://${uri?.split("//")[1]}` ?? "ws://localhost:4000/grapql",
+    uri: process.env.REACT_APP_WEBSOCKET_URL ?? "ws://localhost:4000/grapql",
     options: {
       reconnect: true,
       connectionParams: {
