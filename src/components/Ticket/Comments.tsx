9e9c43d1cdef50d55c027af3a3c0def92028c68b
mismaah <mismaahabdulla@gmail.com>
2022-03-03 11:04:54 +0500

    FIX: Change fetch policy to check network as well since newer comments are not loaded if already cached

--- src/components/Ticket/Comments.tsx
+++ src/components/Ticket/Comments.tsx
@@ -9,6 +9,26 @@ import { COMMENT_CREATED } from "../../api/subscriptions";
 import CommentBubble from "./CommentBubble";
 import CommentGroup from "../../models/CommentGroup";
 
+const groupComments = (data: any) => {
+  let grouped: CommentGroup[] = [];
+  let lastGroup: any = { user: null, comments: [] };
+  data?.comments.forEach((comment: Comment, index: number) => {
+    const commentWithoutUser = { ...comment, user: undefined };
+    if (lastGroup.user === null) {
+      lastGroup.user = comment.user;
+    } else if (comment.user.id !== lastGroup.user?.id) {
+      grouped.push(lastGroup);
+      lastGroup = { user: null, comments: [] };
+      lastGroup.user = comment.user;
+    }
+    lastGroup.comments.push(commentWithoutUser);
+    if (index + 1 === data?.comments.length) {
+      grouped.push(lastGroup);
+    }
+  });
+  return grouped;
+};
+
 const Comments = ({ ticket }: { ticket: Ticket }) => {
   const [subscribed, setSubscribed] = useState(false);
 
@@ -21,6 +41,7 @@ const Comments = ({ ticket }: { ticket: Ticket }) => {
       onError: (err) => {
         errorMessage(err, "Error loading request.");
       },
+      fetchPolicy: "cache-and-network",
     }
   );
   useEffect(() => {
@@ -46,25 +67,7 @@ const Comments = ({ ticket }: { ticket: Ticket }) => {
     setSubscribed(true);
   };
 
-  const groupedComments = () => {
-    let grouped: CommentGroup[] = [];
-    let lastGroup: any = { user: null, comments: [] };
-    data?.comments.forEach((comment: Comment, index: number) => {
-      const commentWithoutUser = { ...comment, user: undefined };
-      if (lastGroup.user === null) {
-        lastGroup.user = comment.user;
-      } else if (comment.user.id !== lastGroup.user?.id) {
-        grouped.push(lastGroup);
-        lastGroup = { user: null, comments: [] };
-        lastGroup.user = comment.user;
-      }
-      lastGroup.comments.push(commentWithoutUser);
-      if (index + 1 === data?.comments.length) {
-        grouped.push(lastGroup);
-      }
-    });
-    return grouped;
-  };
+  const groupedComments = groupComments(data);
 
   return (
     <>
@@ -90,7 +93,7 @@ const Comments = ({ ticket }: { ticket: Ticket }) => {
         }}
       >
         <div>
-          {groupedComments().map((group: CommentGroup) => (
+          {groupedComments?.map((group: CommentGroup) => (
             <CommentBubble group={group} key={group.comments[0].id} />
           ))}
         </div>
