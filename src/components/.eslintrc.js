3fba9cdbbf52e98c2134d79e2c2754e7dfcab357
mismaah <mismaahabdulla@gmail.com>
2022-03-06 14:44:00 +0500

    fix(eslint): missed plugin prefix

--- src/components/.eslintrc.js
+++ src/components/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["prettier/recommended", "plugin:react/recommended"],
+  extends: ["plugin:prettier/recommended", "plugin:react/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
